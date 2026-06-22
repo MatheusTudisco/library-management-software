51dd5d80b950f570486d2655dc055c2286cc3caa
Matheus <matheustudisco@hotmail.com>
2026-06-22 13:58:44 -0300

    feat: estrutura inicial do projeto com maven

---
+++ src/main/java/com/matheustudisco/librarymanagementsoftware/application/Program.java
@@ -0,0 +1,7 @@
+package main.java.com.matheustudisco.librarymanagementsoftware.application;
+
+public class Program {
+        public static void main(String[] args) {
+        System.out.println("Sistema iniciado");
+    }
+}
