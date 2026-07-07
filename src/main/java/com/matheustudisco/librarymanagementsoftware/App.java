b9ff44213ca5001b3e5df842cdd2547289e65926
Matheus Tudisco <matheustudisco@hotmail.com>
2026-07-06 22:09:32 -0300

    Adicionado tratamento de campo vazio no registro de livro

--- src/main/java/com/matheustudisco/librarymanagementsoftware/App.java
+++ src/main/java/com/matheustudisco/librarymanagementsoftware/App.java
@@ -60,7 +60,7 @@ public class App {
                 while (lastName.isEmpty()) {
                     System.out.print("Digite o ultimo nome: ");
                     lastName = scanner.nextLine();
-                    if (lastName.isEmpty()){
+                    if (lastName.isEmpty()) {
                         System.out.println("Erro! O campo último nome não pode estar vazio.");
                     }
                 }
@@ -72,26 +72,26 @@ public class App {
                     }
                 }
                 /*
-                * Para validar a verificação de um campo vazio na digitação, primeiro
-                * inicio a variável dateOfBirth como "null", após entrar no while ele pede
-                * ao usuário que digite a data, salva em uma variável String para poder
-                * validar se o usuário deixe vazia, caso se confirme ele exibe a mensagem de erro
-                * na tela e o "continue" força a retornar para o teste de condição do loop.
-                * Caso o usuário faça a digitação, ele passa para a variável LocalDate
-                * a variável String com o parse e faz a verificação com o DateTimeParseException se
-                * o formato escrito está correto, ou se o usuário digitou algum caractere inválido.
-                * Caso tudo estiver correto ele pula para o próximo item do cadastro.
+                 * Para validar a verificação de um campo vazio na digitação, primeiro
+                 * inicio a variável dateOfBirth como "null", após entrar no while ele pede
+                 * ao usuário que digite a data, salva em uma variável String para poder
+                 * validar se o usuário deixe vazia, caso se confirme ele exibe a mensagem de erro
+                 * na tela e o "continue" força a retornar para o teste de condição do loop.
+                 * Caso o usuário faça a digitação, ele passa para a variável LocalDate
+                 * a variável String com o parse e faz a verificação com o DateTimeParseException se
+                 * o formato escrito está correto, ou se o usuário digitou algum caractere inválido.
+                 * Caso tudo estiver correto ele pula para o próximo item do cadastro.
                  */
-                while (dateOfBirth == null){
+                while (dateOfBirth == null) {
                     System.out.print("Digite a data de nascimento (DD/MM/AAAA): ");
                     String dateOfBirthString = scanner.nextLine();
-                    if (dateOfBirthString.isEmpty()){
+                    if (dateOfBirthString.isEmpty()) {
                         System.out.println("Erro! O campo Data de nascimento não pode estar vazio.");
                         continue;
                     }
-                    try{
+                    try {
                         dateOfBirth = LocalDate.parse(dateOfBirthString, formatter);
-                    }catch (DateTimeException e){
+                    } catch (DateTimeException e) {
                         System.out.println("Erro! Formato inválido ou data inexistente");
                     }
                 }
@@ -102,10 +102,10 @@ public class App {
                         System.out.println("Erro! O campo celular não pode estar vazio.");
                     }
                 }
-                while (email.isEmpty()){
+                while (email.isEmpty()) {
                     System.out.print("Digite o email: ");
                     email = scanner.nextLine();
-                    if (email.isEmpty()){
+                    if (email.isEmpty()) {
                         System.out.println("Erro! O campo email não pode estar vazio.");
                     }
                 }
@@ -113,82 +113,122 @@ public class App {
                 User newUser = new User(name, lastName, cpf, dateOfBirth, cellphone, email);
                 userService.registrationService(newUser);
 
-        } else if (escolha == 2) {
-            System.out.print("Digite o nome do livro: ");
-            String title = scanner.nextLine();
-            System.out.print("Digite o autor: ");
-            String author = scanner.nextLine();
-            System.out.print("Digite o genero do livro: ");
-            String genre = scanner.nextLine();
-            System.out.print("Digite o ano do livro: ");
-            int year = Integer.parseInt(scanner.nextLine());
-            System.out.print("Digite o volume do livro: ");
-            byte volume = Byte.parseByte(scanner.nextLine());
-            System.out.print("Digite a quantidade disponivel: ");
-            short quantity = Short.parseShort(scanner.nextLine());
+            } else if (escolha == 2) {
+                String title = "", author = "", genre = "", yearString = "", volumeString = "", quantityString = "";
+                int year = 0;
+                byte volume = 0;
+                short quantity = 0;
 
-            Book newBook = new Book(title, author, genre, year, volume, quantity);
-            bookService.registrationBook(newBook);
+                while (title.isEmpty()) {
+                    System.out.print("Digite o nome do livro: ");
+                    title = scanner.nextLine();
+                    if (title.isEmpty()) {
+                        System.out.println("Erro! O campo titulo não pode estar vazio.");
+                    }
+                }
+                while (author.isEmpty()) {
+                    System.out.print("Digite o autor: ");
+                    author = scanner.nextLine();
+                    if (author.isEmpty()) {
+                        System.out.println("Erro! O campo autor não pode estar vazio.");
+                    }
+                }
+                while (genre.isEmpty()) {
+                    System.out.print("Digite o genero do livro: ");
+                    genre = scanner.nextLine();
+                    if (genre.isEmpty()) {
+                        System.out.println("Erro! O campo genêro não pode estar vazio.");
+                    }
+                }
+                while (yearString.isEmpty()) {
+                    System.out.print("Digite o ano do livro: ");
+                    yearString = scanner.nextLine();
+                    if (yearString.isEmpty()) {
+                        System.out.println("Erro! O campo ano não pode estar vazio.");
+                    } else {
+                        year = Integer.parseInt(yearString);
+                    }
+                }
+                while (volumeString.isEmpty()) {
+                    System.out.print("Digite o volume do livro: ");
+                    volumeString = scanner.nextLine();
+                    if (volumeString.isEmpty()) {
+                        System.out.println("Erro! O campo volume não pode estar vazio.");
+                    } else {
+                        volume = Byte.parseByte(volumeString);
+                    }
+                }
+                while (quantityString.isEmpty()) {
+                    System.out.print("Digite a quantidade disponivel: ");
+                    quantityString = scanner.nextLine();
+                    if (quantityString.isEmpty()) {
+                        System.out.println("Erro! O campo quantidade não pode estar vazio.");
+                    } else {;
+                        quantity = Short.parseShort(quantityString);
+                    }
+                }
+                Book newBook = new Book(title, author, genre, year, volume, quantity);
+                bookService.registrationBook(newBook);
 
-        } else if (escolha == 3) {
-            //For para "limpar a tela", técnica do pulo de linhas
-            for (int i = 0; i <= 50; i++) {
-                System.out.println();
-            }
+            } else if (escolha == 3) {
+                //For para "limpar a tela", técnica do pulo de linhas
+                for (int i = 0; i <= 50; i++) {
+                    System.out.println();
+                }
 
-            System.out.println(userRepository.showUser());
+                System.out.println(userRepository.showUser());
 
-            System.out.println("Digite 1 para encerrar");
-            System.out.println("Digite 2 para voltar ao menu principal");
-            byte escolhaUser = Byte.parseByte(scanner.nextLine());
+                System.out.println("Digite 1 para encerrar");
+                System.out.println("Digite 2 para voltar ao menu principal");
+                byte escolhaUser = Byte.parseByte(scanner.nextLine());
 
-            if (escolhaUser == 1) {
+                if (escolhaUser == 1) {
+                    selecaoWhile = true;
+                    System.out.println("=====================================");
+                    System.out.println(">>>>>ENCERRANDO O SISTEMA<<<<<");
+                    System.out.println("=====================================");
+                } else if (escolhaUser == 2) {
+                    continue;
+                } else {
+                    System.out.println("Digite uma opção válida");
+                }
+
+            } else if (escolha == 4) {
+                //For para "limpar a tela", técnica do pulo de linhas
+                for (int i = 0; i <= 50; i++) {
+                    System.out.println();
+                }
+                System.out.println(bookRepository.showBook());
+
+                System.out.println("Digite 1 para encerrar");
+                System.out.println("Digite 2 para voltar ao menu principal");
+                byte escolhaBook = Byte.parseByte(scanner.nextLine());
+
+                if (escolhaBook == 1) {
+                    selecaoWhile = true;
+                    System.out.println("=====================================");
+                    System.out.println(">>>>>ENCERRANDO O SISTEMA<<<<<");
+                    System.out.println("=====================================");
+                } else if (escolhaBook == 2) {
+                    continue;
+                } else {
+                    System.out.println("Digite uma opção válida");
+                }
+
+            } else {
                 selecaoWhile = true;
+
+                //For para "limpar a tela", técnica do pulo de linhas
+                for (int i = 0; i <= 50; i++) {
+                    System.out.println();
+                }
+
                 System.out.println("=====================================");
                 System.out.println(">>>>>ENCERRANDO O SISTEMA<<<<<");
                 System.out.println("=====================================");
-            } else if (escolhaUser == 2){
-                continue;
-            } else {
-                System.out.println("Digite uma opção válida");
             }
-
-        } else if (escolha == 4) {
-            //For para "limpar a tela", técnica do pulo de linhas
-            for (int i = 0; i <= 50; i++) {
-                System.out.println();
-            }
-            System.out.println(bookRepository.showBook());
-
-            System.out.println("Digite 1 para encerrar");
-            System.out.println("Digite 2 para voltar ao menu principal");
-            byte escolhaBook = Byte.parseByte(scanner.nextLine());
-
-            if (escolhaBook == 1) {
-                selecaoWhile = true;
-                System.out.println("=====================================");
-                System.out.println(">>>>>ENCERRANDO O SISTEMA<<<<<");
-                System.out.println("=====================================");
-            } else if (escolhaBook == 2){
-                continue;
-            } else {
-                System.out.println("Digite uma opção válida");
-            }
-
-        } else {
-            selecaoWhile = true;
-
-            //For para "limpar a tela", técnica do pulo de linhas
-            for (int i = 0; i <= 50; i++) {
-                System.out.println();
-            }
-
-            System.out.println("=====================================");
-            System.out.println(">>>>>ENCERRANDO O SISTEMA<<<<<");
-            System.out.println("=====================================");
         }
+
+
     }
-
-
-}
 }
