c37ed4cab2499aa44da330b3093ca3be15547795
Matheus Tudisco <matheustudisco@hotmail.com>
2026-07-16 21:49:20 -0300

    Iniciando a classe ENUM

---
+++ src/main/java/com/matheustudisco/librarymanagementsoftware/enums/Genre.java
@@ -0,0 +1,26 @@
+package com.matheustudisco.librarymanagementsoftware.enums;
+
+public enum Genre {
+    FICCAO_CIENTIFICA("Ficção Científica"),
+    FANTASIA("Fantasia"),
+    DISTOPIA("Distopia"),
+    TERROR("Terror/Horror"),
+    SUSPENSE_E_MISTERIO("Suspense e Mistério"),
+    ROMANCE("Romance"),
+    FICCAO_HISTORICA("Ficção Histórica"),
+    AVENTURA("Aventura"),
+    DRAMA("Drama"),
+    CONTO_E_CRONICA("Contos e Crônicas"),
+    POESIA("Poesia"),
+    HQ_E_MANGA("HQ e Manga"),
+    BIOGRAFIA("Biografia e Autobiografia"),
+    HISTORIA("História"),
+    FILOSOFIA("Filosofia"),
+    PSICOLOGIA("Psicologia"),
+    AUTO_AJUDA("Autoajuda"),
+    NEGOCIOS_E_FINANCAS("Negócios e Finanças"),
+    TECNOLOGIA_E_COMPUTACAO("Tecnologia e Computação"),
+    CIENCIAS_EXATAS("Ciências Exatas"),
+    RELIGIAO_E_ESPIRITUALIDADE("Religião e Espiritualidade"),
+    ARTE_E_DESIGN("Arte e Design")
+}
